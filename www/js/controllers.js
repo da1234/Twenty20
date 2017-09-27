7f10c1a7f997bbe9d5c13f64bd4d033ebda3fe55
da1234 <darrell.adjei@hotmail.com>
2017-09-27 23:05:26 +0100

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,18 +15,10 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
 
             //<!-- Setting the maps view based on the longitude and latitude with zoom level 18 -->
             var map = L.map('map').setView([lat, long], 15);
-
-            // heat map layer
-            // lat, lng, intensity (up to 1.0)
-            // COORDINATES OF CRIMES, WITH THEIR INTENSITY (UP TO 1.0)
-
             var locCrimes = Routes.getLocalCrimes(lat,long);
             locCrimes.then(function(crimesArrayResults){
               var crimesArray = crimesArrayResults;
               console.log(JSON.stringify(crimesArray));
-
-
-
               var heat = L.heatLayer(crimesArray, {radius: 100}).addTo(map);
 
               //<!-- z - zoom level, y - longitude, x - latitude -->
@@ -41,8 +33,6 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
             },function(err) {
               console.log("error with getting crime map points");
             });
-
-
           }, function(err) {
           console.log("error with geolocation");
           lat = 51.5257430;
@@ -52,16 +42,12 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
           // heat map layer
           // lat, lng, intensity (up to 1.0)
           // COORDINATES OF CRIMES, WITH THEIR INTENSITY (UP TO 1.0)
-
           var locCrimes = Routes.getLocalCrimes(lat,long);
           locCrimes.then(function(crimesArrayResults){
             var crimesArray = crimesArrayResults;
             console.log(JSON.stringify(crimesArray));
-
-
-
             var heat = L.heatLayer(crimesArray, {radius: 100}).addTo(map);
-
+            
             //<!-- z - zoom level, y - longitude, x - latitude -->
             var osm = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
 
@@ -82,8 +68,6 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
   $scope.SearchRoute = function(locations){
     var routeResults = Routes.getRoute(locations);
     routeResults.then(function(data1){
-
-
     var OrigMoves = data1.legs[0].maneuvers;
     var AltRoutes = data1.alternateRoutes;//[0].route.legs[0].maneuvers;
     var totCrimes= 0;
@@ -94,18 +78,13 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     var BestRoute = data1.name;
 
     ////making a dictionary to compare routes ////
-
     var mMap = new Map();
 
     /////tallying up the no. of crimes in the first route /////
-
     for (var r=0; r<OrigMoves.length; r++){
-
       var startingLat = OrigMoves[r].startPoint.lat;
       var startingLng = OrigMoves[r].startPoint.lng;
-
       var mCrimes = Routes.getCrimes(startingLat, startingLng);
-
       mCrimes.then(function(data2){
          totCrimes = totCrimes + data2.length;
          mMap.set(data1.name, totCrimes);
@@ -113,24 +92,19 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
     };
 
     ////tallying up the alternate routes////
-
     for (var r = 0; r< AltRoutes.length; r++){
       AltMoves = AltRoutes[r].route.legs[0].maneuvers;
       var AltRouteName = AltRoutes[r].route.name;
       totAltCrimes = 0;
-
       for (var s=0; s<AltMoves.length; s++){
         var startingLat = AltMoves[s].startPoint.lat;
         var startingLng = AltMoves[s].startPoint.lng;
-
         var mCrimes = Routes.getCrimes(startingLat, startingLng);
-
         mCrimes.then(function(data2){
           totAltCrimes = totAltCrimes + data2.length;
           mMap.set(AltRouteName, totAltCrimes);
 
           ////comparing the no.  of crimes in both routes //////
-
           mMap.forEach(function(value, key) {
             // console.log(key + " = " + value);
             if(value < LeastCrimes){
@@ -143,9 +117,7 @@ angular.module('starter.controllers', ['ionic', 'ngCordova'])
           $scope.bestRoute = BestRoute;
 
           //Now search through data to find other info about the best route
-
           var altRoutes = data1.alternateRoutes;
-
           if(data1.name == BestRoute){
             var routeInfo = {
               name : data1.name,
